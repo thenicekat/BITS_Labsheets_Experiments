cbb3cbf57ab7387bf66871f54472ef5a1690d120
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-02-13 23:10:12 +0530

    Update I

--- LabSheet3/I.c
+++ LabSheet3/I.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
+#include <limits.h>
 #define int long long
+
+int sum(int a) {
+    return (a*(a+1))/2;
+}
+
+int min(int a, int b){
+    return a < b ? a : b;
+}
+
+int max(int a, int b){
+    return a > b ? a : b;
+}
 
 void main()
 {
     int input;
     scanf("%d", &input);
 
-    int halfWayPoint = (input * (input + 1)) / 4;
+    int halfWayPoint = sum(input-1) / 2;
+    printf("The Halfway point is: %lld\n", halfWayPoint);
 
     int start = 0;
     int end = input - 1;
 
-    while (start < end)
+    int answer = 0;
+
+    while (start <= end)
     {
-        int x = ((end * (end + 1)) / 2) - ((start * (start + 1)) / 2);
-        if (x > halfWayPoint)
-        {
-            if(start != (start+end)/2) start = (start + end) / 2;
+        int mid = (start + end)/2;
+        printf("%lld %lld %lld\n", start, mid, end);
+        int diff = sum(input-1) - sum(mid); 
+        //here we subtract the number from total sum
+        //say we have input as 10
+        //total sum = 45 
+        //mid sum will be 10
+        //diff will be 35
+        //if diff > halfway which here is 22
+        //we consider this
+        //that would mean number is below mid :)
+        if(diff > halfWayPoint){
+            answer = max(answer, mid);
+            start = mid+1;
+        }else{
+            end = mid-1;
         }
-        else break;
     }
 
-    printf("%d", end-start);
+    printf("\n%lld", input-1-answer);
 }

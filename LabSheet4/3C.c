ac3c769987b52f0bd5472a3c41f3610cd4a9770c
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-02-15 14:00:41 +0530

    Update Q3C

--- LabSheet4/3C.c
+++ LabSheet4/3C.c
@@ -2,36 +2,68 @@
 
 #define int long long int
 
-int min(int a, int b){
+int min(int a, int b)
+{
     return a < b ? a : b;
 }
 
-int findNoOfTrailingZeroes(int a){
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int findNoOfTrailingZeroes(int a)
+{
     int fcounter = 0;
     int tcounter = 0;
 
     int twos = 2;
-    while(a/twos > 0){
-        int temp = a/twos;
+    while (a / twos > 0)
+    {
+        int temp = a / twos;
         tcounter += temp;
         twos *= 2;
-    }  
+    }
 
     int fives = 5;
-    while(a/fives > 0){
-        int temp = a/fives;
+    while (a / fives > 0)
+    {
+        int temp = a / fives;
         fcounter += temp;
         fives *= 5;
-    }  
+    }
 
     return min(fcounter, tcounter);
 }
 
-void main(){
-    FILE* file;
+void main()
+{
+    FILE *file;
     file = fopen("3C.txt", "r");
 
-    int zeroes;
-    
-    printf("%d", findNoOfTrailingZeroes(1000000));
+    int num, maxNumber = 0;
+    fscanf(file, "%lld", &num);
+
+    int input[num];
+    for (int i = 0; i < num; i++)
+    {
+        fscanf(file, "%lld", &input[i]);
+        maxNumber = max(maxNumber, input[i]);
+    }
+
+    int zeroes[maxNumber];
+    for (int i = 0; i < maxNumber; i++)
+    {
+        zeroes[i] = 0;
+    }
+    for(int i = 0; i < num; i++){
+        zeroes[findNoOfTrailingZeroes(input[i])]++;
+    }
+
+    int answer = 0;
+    for(int i=0; i<maxNumber; i++){
+        int n = zeroes[i];
+        answer += (n*(n-1))/2;
+    }
+    printf("%lld", answer);
 }

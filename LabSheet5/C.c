7b657ee03e6f933207b72c6a623e0200814ee34e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-02-21 22:21:50 +0530

    Add QC

---
+++ LabSheet5/C.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define int long long int
+
+void main()
+{
+    FILE *file;
+    file = fopen("C.txt", "r");
+
+    int len, alphabet;
+    fscanf(file, "%lld %lld", &len, &alphabet);
+
+    char input[len];
+    fscanf(file, "%s", input);
+    printf("%s\n", input);
+
+    int alphabetStore[alphabet];
+
+    int end = alphabet;
+
+    int answer = 0;
+    for (int start = 0; start < len - alphabet + 1; start++)
+    {
+        // Initializing array to 0
+        for (int i = 0; i < alphabet; i++)
+        {
+            alphabetStore[i] = 0;
+        }
+
+        // Reading characters in the current window
+        for (int i = start; i < end; i++)
+        {
+            if ((input[i] - 'a') < alphabet)
+                alphabetStore[input[i] - 'a']++;
+        }
+
+        // Checking if it satisfies all the letters
+        int counter = 0;
+        for (int i = 0; i < alphabet; i++)
+        {
+            if (alphabetStore[i] > 0)
+                counter++;
+        }
+        if (counter == alphabet)
+        {
+            answer += len - end + 1;
+        }
+
+        // Incrementing end
+        end++;
+    }
+
+    printf("%lld", answer);
+}

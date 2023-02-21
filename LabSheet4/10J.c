87b2a80108d9d8961f9045c0db40da417b47da6b
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-02-21 13:25:42 +0530

    Update Labsheet 4

--- LabSheet4/10J.c
+++ LabSheet4/10J.c
@@ -2,22 +2,26 @@
 
 #define int long long int
 
-int absolute(int x){
+int absolute(int x)
+{
     return x > 0 ? x : -x;
 }
 
-int* mergeSortedLists(int* input, int start, int mid, int end){
+int *mergeSortedLists(int *input, int start, int mid, int end)
+{
     int lengthOfFirstHalf = mid - start + 1;
     int lengthOfSecondHalf = end - mid;
 
     int left[lengthOfFirstHalf];
     int right[lengthOfSecondHalf];
 
-    for(int i=0; i<lengthOfFirstHalf; i++){
+    for (int i = 0; i < lengthOfFirstHalf; i++)
+    {
         left[i] = input[start + i];
     }
-    
-    for(int i=0; i<lengthOfSecondHalf; i++){
+
+    for (int i = 0; i < lengthOfSecondHalf; i++)
+    {
         right[i] = input[mid + i + 1];
     }
 
@@ -25,12 +29,16 @@ int* mergeSortedLists(int* input, int start, int mid, int end){
     int rightTracker = 0;
     int tracker = start;
 
-    while(leftTracker < lengthOfFirstHalf && rightTracker < lengthOfSecondHalf){
-        if(left[leftTracker] < right[rightTracker]){
+    while (leftTracker < lengthOfFirstHalf && rightTracker < lengthOfSecondHalf)
+    {
+        if (left[leftTracker] < right[rightTracker])
+        {
             input[tracker] = left[leftTracker];
             leftTracker++;
             tracker++;
-        }else{
+        }
+        else
+        {
             input[tracker] = right[rightTracker];
             rightTracker++;
             tracker++;
@@ -50,38 +58,58 @@ int* mergeSortedLists(int* input, int start, int mid, int end){
         rightTracker++;
         tracker++;
     }
+}
 
-} 
-
-void mergeSort(int* input, int start, int end){
-    if(start < end){
-        int mid = (start + end)/2;
+void mergeSort(int *input, int start, int end)
+{
+    if (start < end)
+    {
+        int mid = (start + end) / 2;
         mergeSort(input, start, mid);
-        mergeSort(input, mid+1, end);
+        mergeSort(input, mid + 1, end);
         mergeSortedLists(input, start, mid, end);
     }
 }
 
-void main(){
-    FILE* file;
+void main()
+{
+    FILE *file;
     file = fopen("10J.txt", "r");
-    
+
     int num;
     fscanf(file, "%lld", &num);
 
     int input[num];
     int totalSum = 0;
-    for(int i=0; i<num; i++){
+    for (int i = 0; i < num; i++)
+    {
         fscanf(file, "%lld", &input[i]);
+        totalSum += input[i];
     }
 
-    mergeSort(input, 0, num-1);
+    mergeSort(input, 0, num - 1);
 
-    int median = input[num/2];
+    int median = input[num / 2];
+    int average = totalSum / num;
+
+    printf("Total Available Chocolates: %lld\n", totalSum);
 
     int answer = 0;
-    for(int i=0; i<num; i++){
-        answer += absolute(input[i] - median);
+
+    if (median * num < totalSum)
+    {
+            printf("Chocolates Per Person: %lld\n", median) ;   
+        for (int i = 0; i < num; i++){
+            answer += absolute(input[i] - median);
+        }
     }
-    printf("%lld", answer);
+    else
+    {
+            printf("Chocolates Per Person: %lld\n", average) ;   
+        for (int i = 0; i < num; i++){
+            answer += absolute(input[i] - average);
+        }
+    }
+
+    printf("No of Operations: %lld\n", answer);
 }

82687314104d96f7883ea12d49328c4f3ce3471e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-02-23 10:31:16 +0530

    Update A such that it counts also

--- LabSheet5/A.c
+++ LabSheet5/A.c
@@ -2,6 +2,28 @@
 
 #define int long long int
 
+void parenthesisCounter(char* answer, int curr, int open, int close, int max, int* counter)
+{
+    if (close == max && open == max)
+    {
+        (*counter)++;
+        return ;
+    }
+    else
+    {
+        if (open < max)
+        {
+            answer[curr] = '(';
+            parenthesisCounter(answer, curr + 1, open + 1, close, max, counter);
+        }
+        if (open > close)
+        {
+            answer[curr] = ')';
+            parenthesisCounter(answer, curr + 1, open, close + 1, max, counter);
+        }
+    }
+}
+
 void printParenthesis(char* answer, int curr, int open, int close, int max)
 {
     if (close == max && open == max)
@@ -33,5 +55,10 @@ void main()
     fscanf(file, "%lld", &length);
 
     char answer[100];
+    int answerCount = 0;
+
+    parenthesisCounter(answer, 0, 0, 0, length / 2, &answerCount);
+
+    printf("%lld\n", answerCount);
     printParenthesis(answer, 0, 0, 0, length / 2);
 }

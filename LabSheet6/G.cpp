ec582583b76724f20134883742524cb68c776175
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-03-02 15:05:25 +0530

    Add H

--- LabSheet6/G.cpp
+++ LabSheet6/G.cpp
@@ -5,9 +5,50 @@
 // 110 85 65 73 65 78 89
 
 // First, we take 110 and print 1
-// next we loop and see if the number is smaller, if yes, we make it zero 
+// next we loop and see if the number is smaller, if yes, we make it zero
 // and do ++ again
 // 1 1 1 2 1 2 3
 
 // Hm exactly this won't work, we need to think of something more
-// What if we keep track of 
+// What if we keep track of
+
+// Stack: 110 counter = 1
+// Answer: 1
+// Stack 110 85 counter = 1
+// Answer: 1
+// Stack 110 85 65 counter = 1
+// Answer: 1
+// Stack 110 85 65 73 -> 110 85 73 counter = 1
+//
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int32_t main()
+{
+    int counter;
+    cin >> counter;
+
+    int input[counter];
+    for (int i = 0; i < counter; i++)
+    { 
+        cin >> input[i];
+    }
+
+    stack<int> stack_sec;
+    for(int i=0; i<counter; i++){
+        int t = 0;
+        while(!stack_sec.empty() && stack_sec.top() < input[i]){
+            stack_sec.pop();
+            t++;
+        }
+
+        if(stack_sec.empty()){
+            cout << 1 << " ";
+        }else{
+            
+        }
+    }
+}

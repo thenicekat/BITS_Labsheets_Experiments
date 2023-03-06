1f88822d91df73179987442c689f1f4850ee68b7
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-03-06 15:35:07 +0530

    Fix tasks.json and add rbt file

---
+++ Practice/reb_black_tree.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t
+{
+    int key;
+    enum color_t
+    {
+        RED,
+        BLACK
+    } color;
+    struct node_t *left;
+    struct node_t *right;
+    struct node_t *parent;
+} Node;
+
+Node *createNode(int val)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->key = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->parent = NULL;
+    newNode->color = RED;
+    return newNode;
+}
+
+void leftRotate(Node **root, Node *x)
+{
+    Node *toBeRoot = x->right;
+    if (toBeRoot->left != NULL)
+    {
+        Node *toBeLeftRight = toBeRoot->left;
+        x->right = toBeLeftRight;
+        toBeRoot->left = NULL;
+    }
+    else
+    {
+        x->right = NULL;
+    }
+
+    toBeRoot->left = x;
+
+    if(x->parent == NULL){
+        *root = toBeRoot;
+    }else if(x->parent->left == x){
+        x->parent->left = toBeRoot;
+    }else{
+        x->parent->right = toBeRoot;
+    }
+    
+    x->parent = toBeRoot;
+}

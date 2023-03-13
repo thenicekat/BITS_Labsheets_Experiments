1992d03f0d212daeaa3d08590fc8e56005c82692
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-03-13 15:39:38 +0530

    Rename folders

---
+++ LabSheet7_Tree/A.cpp
@@ -0,0 +1,3 @@
+// I think the logic is we sort the numbers using merge sort which gives us nlogn
+// then we do max log per number and this gives us mlogn cause idk
+// just assuming
---
+++ LabSheet7_Tree/B.cpp
@@ -0,0 +1,2 @@
+// We use recursion, keep finding roots and the parts which are to the left and 
+// which are to the right and continue :)

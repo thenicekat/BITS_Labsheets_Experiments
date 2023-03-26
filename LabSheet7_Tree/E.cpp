30d501faffbefed6b28186ff26f9a831aacd0ea3
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-03-26 14:55:27 +0530

    update e

--- LabSheet7_Tree/E.cpp
+++ LabSheet7_Tree/E.cpp
@@ -1,7 +1,9 @@
 // Me dumb, apparently inorder always gives you a sorted list :)
-// should have known this before midsems 
+// should have known this before midsems
 // legit regret life right now
 
+// ok inorder is wrong because time complexity will o(all number of nodes)
+// sad
 #include <iostream>
 using namespace std;
 
@@ -15,41 +17,56 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-TreeNode* convert_to_tree(int* arr, int index, int max){
-    TreeNode* root = nullptr;
-    if(index < max && arr[index] != -1){
+TreeNode *convert_to_tree(int *arr, int index, int max)
+{
+    TreeNode *root = nullptr;
+    if (index < max && arr[index] != -1)
+    {
         root = new TreeNode(arr[index]);
-        root->left = convert_to_tree(arr, 2*index + 1, max);
-        root->right = convert_to_tree(arr, 2*index + 2, max);
+        root->left = convert_to_tree(arr, 2 * index + 1, max);
+        root->right = convert_to_tree(arr, 2 * index + 2, max);
     }
     return root;
 }
 
-void inorder(TreeNode* root, int *result, int* counter){
-    if(root == nullptr) return;
-    inorder(root->left, result, counter);
-    result[(*counter)++] = root->val;
-    inorder(root->right, result, counter);
+void inorder(TreeNode *root, int *answerCounter, int* found)
+{
+    // if root is null return
+    if (root == nullptr || *found == 1) return;
+
+    // go to the right nodes
+    inorder(root->right, answerCounter, found);
+
+    //Check if the answer counter is 0
+    if (--(*answerCounter) == 0)
+    {
+        cout << "Answer: " << root->val << endl;
+        *found = 1;
+    }
+    else
+    {
+        inorder(root->left, answerCounter, found);
+    }
 }
 
-int main(){
+int main()
+{
     freopen("Inputs/E.txt", "r", stdin);
+
     int counter;
     cin >> counter;
-    
+
     int input[counter];
-    for (int i = 0; i < counter; i++){
+    for (int i = 0; i < counter; i++)
+    {
         cin >> input[i];
     }
 
     int t;
-    cin >> t; 
+    cin >> t;
 
-    TreeNode* tree = convert_to_tree(input, 0, counter);
-    
-    int temp_counter = 0;
-    int array[counter];
-    inorder(tree, array, &temp_counter);
+    TreeNode *tree = convert_to_tree(input, 0, counter);
 
-    cout << array[temp_counter - t];
+    int found = 0;
+    inorder(tree, &t, &found);
 }

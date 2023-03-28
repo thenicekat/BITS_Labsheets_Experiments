a65d1ef7e6171c7398bdaf661bdfd6fc0b92dddc
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-03-28 10:25:07 +0530

    Fix skill issue

--- LabSheet7_Tree/D.cpp
+++ LabSheet7_Tree/D.cpp
@@ -1,4 +1,4 @@
-// We observe that all numbers to the right of 
+// We observe that all numbers to the right of
 // node are greater than the node itself
 // So if we start iterating from the root
 // We recursively go to the right most node and start backtracking
@@ -22,55 +22,76 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-TreeNode* convert_to_tree(int* array, int index, int max){
-    TreeNode* root = nullptr;
-    if(index < max && array[index] != -1){
+TreeNode *convert_to_tree(int *array, int index, int max)
+{
+    TreeNode *root = nullptr;
+    if (index < max && array[index] != -1)
+    {
         root = new TreeNode(array[index]);
-        root->left = convert_to_tree(array, 2*index + 1, max);
-        root->right = convert_to_tree(array, 2*index + 2, max);
+        root->left = convert_to_tree(array, 2 * index + 1, max);
+        root->right = convert_to_tree(array, 2 * index + 2, max);
     }
     return root;
 }
 
-void bst_to_gst(TreeNode* root, int* tracker){
+void bst_to_gst(TreeNode *root, int *tracker)
+{
     // First we go to the right sub tree nodes
-    if(root->right != nullptr) bst_to_gst(root->right, tracker);
+    if (root->right != nullptr)
+        bst_to_gst(root->right, tracker);
     // Then to the current node itself
     (*tracker) += root->val;
     // Then we assign the value to the node
     root->val = *tracker;
     // Then iterate through the left sub trees
-    if(root->left != nullptr) bst_to_gst(root->left, tracker);
+    if (root->left != nullptr)
+        bst_to_gst(root->left, tracker);
 }
 
-int main(){
+int main()
+{
     freopen("Inputs/D.txt", "r", stdin);
     int counter;
     cin >> counter;
 
     int input[counter];
 
-    for (int i = 0; i < counter; i++){
+    for (int i = 0; i < counter; i++)
+    {
         cin >> input[i];
     }
 
-    TreeNode* tree = convert_to_tree(input, 0, counter);
+    TreeNode *tree = convert_to_tree(input, 0, counter);
 
     int tracker = 0;
     bst_to_gst(tree, &tracker);
 
     int length = counter;
-    TreeNode* array[length];
+    TreeNode *array[length];
     array[0] = tree;
-    for(int i=0; i<length/2; i++){
-        if(2*i + 1 < counter) array[2*i + 1] = array[i]->left;
-        if(2*i + 2 < counter) array[2*i + 2] = array[i]->right;
+    for (int i = 0; i < length / 2; i++)
+    {
+        // if (array[i] == nullptr)
+        // {
+        //     array[2 * i + 1] = nullptr;
+        //     array[2 * i + 2] = nullptr;
+        // }
+        // else
+        // {
+            if (2 * i + 1 < counter)
+                array[2 * i + 1] = array[i]->left;
+            if (2 * i + 2 < counter)
+                array[2 * i + 2] = array[i]->right;
+        // }
     }
 
-    for(auto x: array){
-        if(x == nullptr){
+    for (auto x : array)
+    {
+        if (x == nullptr)
+        {
             cout << -1 << " ";
         }
-        else cout << x->val << " ";
+        else
+            cout << x->val << " ";
     }
 }

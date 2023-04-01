6fade7104883f58f3a2c324da2a00d6d14346934
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-04-01 12:51:45 +0530

    Add A

--- LabSheet8_MapsSets/A.cpp
+++ LabSheet8_MapsSets/A.cpp
@@ -5,3 +5,56 @@
 // we have eat/tea and ate -> all of them will end up being the same string
 // but the thing is time complexity
 // O(mn [for looping through all letters] + nlogn [for adding them into map] + 26n [for building the hash])
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    freopen("Inputs/A.txt", "r", stdin);
+    int counter;
+    cin >> counter;
+
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "----------- TEST CASE " << i + 1 << " -----------" << endl;
+
+        int noOfwords;
+        cin >> noOfwords;
+
+        map<string, int> store;
+
+        for (int i = 0; i < noOfwords; i++)
+        {
+            string word;
+            cin >> word;
+
+            int letters[26] = {0};
+            for(char t: word){
+                letters[t - 'a']++;
+            }
+
+            string key = "";
+            for(int i=0; i<26; i++){
+                key += ('a' + i);
+                key += to_string(letters[i]);
+            }
+
+            if(store.find(key) == store.end()){
+                store[key] = 1;
+            }else{
+                store[key]++;
+            }
+        }
+
+        int answer = 0;
+        for(auto x: store){
+            answer += (x.second)*(x.second - 1)/2;
+        }
+        cout << answer << endl;
+
+        cout << "--------- END TEST CASE "
+             << " ---------" << endl;
+    }
+}
---
+++ LabSheet8_MapsSets/J.cpp
@@ -0,0 +1,7 @@
+// Hm let's say input is 1 0 0 1 1
+// We have 2 as the limit
+// so we have a variable sliding window thing
+// where we go from  (1 0 0 1) 1 -> 4C2 = 6 + 4 = 10
+// After this it becomes 1 0 0 (1 1) -> 2C2 + 2 = 4
+
+//if input is 1 1 1 1 -> We get 4C2 + 4 = 10

f725b5ec266313f4ba69a64bce18f7c851c279d1
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-04-03 14:31:32 +0530

    Add J I think

--- LabSheet8_MapsSets/B.cpp
+++ LabSheet8_MapsSets/B.cpp
@@ -3,3 +3,58 @@
 // so if we have 1223 -> 1221 and since this is a smaller number, we get 1331
 // for 23545 -> 23542 -> 23532 -> 23632
 // if we have 1234 -> 1231 -> 1221 -> 1331
+// Edge Case 
+// 1
+// 9 -> should give 11
+
+// Split test cases whenever we can
+// Odd and Even cases :)
+// In odd length, we have middle element which can be anything
+// In even, it's mirrored about center
+
+#include <iostream>
+using namespace std;
+
+int isPalindrome(string x){
+    int result = 1;
+    for(int i=0; i<x.size()/2; i++){
+        if(x[i] != x[x.size() - 1]){
+            result = 0;
+        }
+    }
+    return result;
+}
+
+string addOneToMiddle(string x){
+    if(x.size()%2 == 0){
+        // even length meaning 1331
+        // size is 4
+        // we need 1 index so
+        int mid = x.size() - 1;
+        if(x[mid + 1] != '9'){
+            
+        }
+    }
+}
+
+int main(){
+    freopen("Inputs/B.txt", "r", stdin);
+    int counter;
+    cin >> counter;
+
+    for (int i = 0; i < counter; i++){
+        cout << "----------- TEST CASE " << i + 1 << " -----------" << endl;
+
+        int len;
+        cin >> len;
+
+        string input;
+        cin >> input;
+
+        if(isPalindrome(input)){
+            cout << addOneToMiddle(input) << endl;
+        }
+
+        cout << "--------- END TEST CASE " << " ---------" << endl;
+    }
+}
--- LabSheet8_MapsSets/C.cpp
+++ LabSheet8_MapsSets/C.cpp
@@ -1,4 +1,4 @@
-// Given array 3 5 1 4 6 6 
+// Given array 3 5 1 4 6 6
 // Index wise 0:3 1:5 2:1 3:4 4:6 5:6
 // x is 2 and y is -1
 // They want aj - ai = yj - xi
@@ -8,5 +8,45 @@
 // these operations would be O(n)
 // after this we can use two maps and add to the map
 // in cpp map is O(logn)
-// so we get nlogn operations to check if any is in both elements then we can 
+// so we get nlogn operations to check if any is in both elements then we can
 // multiply and add
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    freopen("Inputs/C.txt", "r", stdin);
+    int counter;
+    cin >> counter;
+
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "----------- TEST CASE " << i + 1 << " -----------" << endl;
+
+        int num;
+        cin >> num;
+
+        int input[num];
+        for (int i = 0; i < num; i++)
+            cin >> input[i];
+
+        int x, y;
+        cin >> x >> y;
+
+        int inputy[num];
+        for (int i = 0; i < num; i++)
+        {
+            inputy[i] = input[i] - y*i;
+        }
+
+        int inputx[num];
+        for (int i = 0; i < num; i++)
+        {
+            inputx[i] = input[i] - x*i;
+        }
+
+        cout << "--------- END TEST CASE "
+             << " ---------" << endl;
+    }
+}

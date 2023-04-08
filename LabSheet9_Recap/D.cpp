a7e20b6b02bde0b4e5e990c5e68e96ae020e87b4
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-04-08 17:20:37 +0530

    Add D

--- LabSheet9_Recap/D.cpp
+++ LabSheet9_Recap/D.cpp
@@ -11,3 +11,63 @@
 // which is [2, 2]
 
 // Hmm: https://www.geeksforgeeks.org/count-of-subarrays-with-average-k/
+
+// Let there be a subarray [L, R] whose average is equal to K, then
+// => K = average[L, R] = sum[0, R] – sum[0, L-1] / (R – L + 1)
+// => (R – L + 1) * K = sum[0, R] – sum[0, L – 1]
+// => R * k – (L – 1)* K = sum[0, R] – sum[0, L – 1]
+// => sum[0, R] – R * k = sum[0, L – 1]  – (L – 1)* K
+
+// If every element is decreased by K, then the average will also decrease by K. Therefore, the average can be reduced to zero, so the problem becomes finding the number of subarrays having average equals zero.
+// The average zero is possible only if:
+// sum[0, R] – sum[0, L-1] / (R – L + 1) = 0
+// => sum[0, R] = sum[0, L-1]
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("Inputs/D.txt", "r", stdin);
+    int counter;
+    cin >> counter;
+
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "----------- TEST CASE " << i + 1 << " -----------" << endl;
+
+        // Take input
+        int inLen, average;
+        cin >> inLen >> average;
+        int input[inLen];
+        for (int i = 0; i < inLen; i++)
+        {
+            int x;
+            cin >> x;
+
+            input[i] = x - average;
+        }  
+
+        int answer = 0;
+
+        int prefixSum = 0;
+        map<int, int> prefixFreq;
+        for (int i = 0; i < inLen; i++)
+        {
+            prefixSum += input[i];
+            
+            if(prefixSum == 0){
+                answer++;
+            }
+            
+            answer += prefixFreq[prefixSum];
+
+            prefixFreq[prefixSum]++;
+        }
+
+        cout << answer << endl;
+
+        cout << "--------- END TEST CASE "
+             << " ---------" << endl;
+    }
+}

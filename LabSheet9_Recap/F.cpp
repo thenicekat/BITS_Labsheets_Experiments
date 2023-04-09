c875e64b0d02447effad0bb7307d918e1a42b232
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-04-09 22:35:35 +0530

    Add F

---
+++ LabSheet9_Recap/F.cpp
@@ -0,0 +1,39 @@
+// Maybe we can use upper bound and lower bound?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("Inputs/F.txt", "r", stdin);
+    int counter;
+    cin >> counter;
+
+    for (int i = 0; i < counter; i++){
+        cout << "----------- TEST CASE " << i + 1 << " -----------" << endl;
+
+        int inLen, remK;
+        cin >> inLen >> remK;
+
+        int input[inLen];
+        map<int, int> store;
+        for(int i=0; i<inLen; i++){
+            cin >> input[i];
+            store[input[i]] = 0;
+        }
+
+        int answer = 0;
+
+        for(int i=0; i<inLen; i++){
+            auto iterator = store.upper_bound(input[i] + remK - 1);
+            if(iterator != store.end()){
+                cout << input[i] << " -> " << (*iterator).first << endl;
+                store.erase(iterator);
+                answer++;
+            }
+        }
+
+        cout << answer << endl;
+
+        cout << "--------- END TEST CASE " << " ---------" << endl;
+    }
+}

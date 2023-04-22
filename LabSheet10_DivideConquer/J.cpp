6f91771a99eacd1e623532bc9ffda6e6fbf3e00a
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-04-22 14:03:37 +0530

    Update code of J

--- LabSheet10_DivideConquer/J.cpp
+++ LabSheet10_DivideConquer/J.cpp
@@ -18,27 +18,39 @@ int main()
 
         int dim, k;
         cin >> dim >> k;
+        int input[dim][dim];
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>>
+            store;
 
-        // Am not entirely sure of this but let's see
-        // this will be nlogn
-        priority_queue<int, vector<int>, greater<int>> store;
         for (int i = 0; i < dim; i++)
         {
             for (int j = 0; j < dim; j++)
             {
-                int x;
-                cin >> x;
-                store.push(x);
+                cin >> input[i][j];
             }
         }
 
+        // We push only n so complexity will be nlogn
+        for (int i = 0; i < dim; i++)
+        {
+            store.push({input[0][i], {0, i}});
+        }
+
         // this will be klogn
-        for(int i=0; i<k-1; i++){
+        for (int i = 0; i < k - 1; i++)
+        {
+            pair<int, pair<int, int>> cur = store.top();
             store.pop();
+
+            // We push same 
+            store.push({input[cur.second.first + 1][cur.second.second], {cur.second.first + 1, cur.second.second}});
         }
 
         // this will be O(1)
-        cout << store.top() << endl;
+        cout << store.top().first << endl;
 
         cout << "--------- END TEST CASE "
              << " ---------" << endl;

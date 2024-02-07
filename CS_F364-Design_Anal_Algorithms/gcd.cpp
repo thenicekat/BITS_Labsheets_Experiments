0c79a40d808148670cda69f3c56aa680f6872eff
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-02-07 19:51:49 +0530

    Add gcd also

--- CS_F364-Design_Anal_Algorithms/gcd.cpp
+++ CS_F364-Design_Anal_Algorithms/gcd.cpp
@@ -1,16 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int customGCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    cout << a << " = " << b << " * " << a / b << " + " << int(a % b) << endl;
+    return customGCD(b, a % b);
+}
+
 int main()
 {
     freopen("gcd.txt", "r", stdin);
     int counter;
     cin >> counter;
-
+    cout << "Euclidean Algorithm for GCD" << endl;
     for (int m = 0; m < counter; m++)
     {
         cout << "----------- TEST CASE " << m + 1 << " -----------" << endl;
 
+        int a, b;
+        cin >> a >> b;
+
+        cout << customGCD(a, b) << endl;
         cout << "--------- END TEST CASE "
              << " ---------" << endl;
     }

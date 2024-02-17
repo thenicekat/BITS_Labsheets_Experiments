a81be723b0bb8801ebebc808ae652e747d4d626f
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-02-17 12:49:12 +0530

    smol progress in convex hull

--- CS_F364-Design_Anal_Algorithms/3.ConvexHull/convexhull.cpp
+++ CS_F364-Design_Anal_Algorithms/3.ConvexHull/convexhull.cpp
@@ -3,13 +3,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+float find_median(vector<int> input)
+{
+    // NOTE: this is not the fastest median algorithm
+    // this is nlogn
+    sort(input.begin(), input.end());
+    return input.size() % 2 == 1 ? input[input.size() / 2] : (float)(input[input.size() / 2 - 1] + input[input.size() / 2]) / 2;
+}
+
+pair<pair<int, int>, pair<int, int>> upperbridge(set<pair<int, int>> vertices, float median_x_coord)
+{
+    // We should ideally take input as T the set and L the line
+    // but here we take x = mxc as the line so we just pass the median coord
+    // This should return a line
+}
+
 void upperhull(pair<int, int> pmin, pair<int, int> pmax, set<pair<int, int>> vertices)
 {
+    cout << "::> Running upper hull" << endl;
+    // Add all the x coordinates into the vector
+    vector<int> all_x_coords;
+    for (auto vertice : vertices)
+    {
+        all_x_coords.push_back(vertice.first);
+    }
+    // Find median
+    float median_x_coord = find_median(all_x_coords);
+    // find the upper bridge line
+    pair<pair<int, int>, pair<int, int>> upper_bridge = upperbridge(vertices, median_x_coord);
 }
 
 int main()
 {
     freopen("convexhull_input.txt", "r", stdin);
+    freopen("convexhull_output.txt", "w", stdout);
+
     int counter;
     cin >> counter;
 
@@ -58,13 +86,14 @@ int main()
         cout << "::> P_min: " << pmin.first << ',' << pmin.second << endl;
         cout << "::> P_max: " << pmax.first << ',' << pmax.second << endl;
 
-        cout << "::> Set T - which contains all the points between x(pmin) and x(pmax)" << endl;
+        cout << "::> Set T - which contains all the points between x(pmin) and x(pmax): " << endl;
         set<pair<int, int>> T;
         for (auto coord : coords)
         {
             if (pmin.first < coord.first && coord.first < pmax.first)
             {
                 T.insert(coord);
+                cout << "::> " << coord.first << "," << coord.second << endl;
             }
         }
         upperhull(pmin, pmax, T);
